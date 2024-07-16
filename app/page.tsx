dfe2a9d0069164e200a3f1a7689711cc98e46b13
mishaal-muzammil <mishaalmuzammil@gmail.com>
2024-07-17 03:24:55 +0530

    Renames

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "../components/ThemeToggle";
-import HeroSection from "@/components/containers/landing/HeroSection";
+import HeroSection from "@/components/containers/landing-page/HeroSection";
 
 export default async function LandingPage() {
   return (
