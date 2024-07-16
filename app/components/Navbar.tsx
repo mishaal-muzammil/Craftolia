08c4faccbb08be0b8a97ad161f1adb99e6221650
mishaal-muzammil <mishaalmuzammil@gmail.com>
2024-07-17 01:03:20 +0530

    Temp navbar done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,41 @@
 import Link from "next/link";
+import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "@/components/ui/button";
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar() {
+
+export async function Navbar() {
+    const {isAuthenticated} = getKindeServerSession();
     return(
         <nav className="border-b bg-background h-[10vh] flex items-center">
             <div className="container flex items-center justify-between">
                 <Link href="/">
-                    <h1>Craftolia</h1>
+                    <h1 className="font-bold text-3xl">Craftolia</h1>
                 </Link>
+
+                <div className="flex items-center gap-x-3">
+                    <ThemeToggle/>
+
+                    
+
+                        {(await isAuthenticated()) ? (
+                            <div className="flex items-center gap-x-3">
+                                <Button variant="outline" asChild>
+                                    <Link href="/dashboard">Dashboard</Link>
+                                </Button>
+                                <LogoutLink><Button variant="outline">Logout</Button></LogoutLink>
+                            </div>
+                        ) : (
+                            <div className="flex items-center gap-x-3">                       
+                                <RegisterLink><Button variant="secondary">Sign Up</Button></RegisterLink>
+                                <LoginLink><Button>Sign In</Button></LoginLink>
+                            </div>
+
+                        )}
+                        
+                    </div>            
+                
             </div>
         </nav>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ import { ThemeToggle } from "./components/ThemeToggle";
 
 export default function Home() {
   return (
-    <ThemeToggle/>
+    <></>
   );
 }
