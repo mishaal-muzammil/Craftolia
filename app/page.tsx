e2759a2f0a149b874f5b512adfe89aff972d9eea
mishaal-muzammil <mishaalmuzammil@gmail.com>
2024-07-16 17:48:34 +0530

    shadcn ui setup and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "./components/ThemeToggle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-       
-      </div>
-    </main>
+    <ThemeToggle/>
   );
 }
